1971306c0939f6fa233bb7cffd55271fa2a8d6ab
Riam <riamstefeson616@gmail.com>
2024-08-29 18:45:53 -0300

    Instalando axios e react-router-dom, criando arquivos e rotas de register, login e profile.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' Component={Register}/>
+        <Route path='/login' Component={Login}/>
+        <Route path='/profile' Component={Profile}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
